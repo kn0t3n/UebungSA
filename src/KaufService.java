60ef8f0c53b0a1c2be21d89a4d3f62afaaac6601
knoten <philipp.schweiger85@googlemail.com>
2017-12-05 09:17:27 +0100

    .

--- src/KaufService.java
+++ src/KaufService.java
@@ -17,25 +17,24 @@ public class KaufService {
     }
 
     public void saveOneElement(Kauf kauf) throws SQLException {
+        this.setPreparedInputStatement(kauf);
+        preparedStatementInput.execute();
+    }
+
+    public void saveAllElements(List<Kauf> kaufDBS) throws SQLException {
+        for (Kauf kauf :kaufDBS) {
+            setPreparedInputStatement(kauf);
+            preparedStatementInput.execute();
+        }
+    }
+
+    public void setPreparedInputStatement(Kauf kauf) throws SQLException {
         preparedStatementInput.setString(1, kauf.getPerson().getGeburtsdatum());
         preparedStatementInput.setString(2, kauf.getPerson().getNachname());
         preparedStatementInput.setString(3, kauf.getPerson().getVorname());
         preparedStatementInput.setLong(4, kauf.getKaufDatum());
         preparedStatementInput.setInt(5, kauf.getGoldInGramm());
         preparedStatementInput.setDouble(6, kauf.getGoldImWertVonEuro());
-        preparedStatementInput.execute();
-    }
-
-    public void saveAllElements(List<Kauf> kaufDBS) throws SQLException {
-        for (Kauf kauf :kaufDBS) {
-            preparedStatementInput.setString(1, kauf.getPerson().getGeburtsdatum());
-            preparedStatementInput.setString(2, kauf.getPerson().getNachname());
-            preparedStatementInput.setString(3, kauf.getPerson().getVorname());
-            preparedStatementInput.setLong(4, kauf.getKaufDatum());
-            preparedStatementInput.setInt(5, kauf.getGoldInGramm());
-            preparedStatementInput.setDouble(6, kauf.getGoldImWertVonEuro());
-            preparedStatementInput.execute();
-        }
     }
 
 
